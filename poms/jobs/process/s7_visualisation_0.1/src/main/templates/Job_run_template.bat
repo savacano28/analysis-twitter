b68baec29e78ab4ed2a099a5aa98b95961441914
savacano28 <savacano_28@live.com>
2020-12-06 16:09:57 +0100

    workflow

---
+++ poms/jobs/process/s7_visualisation_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;s7_visualisation_0_1.jar; analysis_twitter.s7_visualisation_0_1.s7_visualisation  --context=Default %*
