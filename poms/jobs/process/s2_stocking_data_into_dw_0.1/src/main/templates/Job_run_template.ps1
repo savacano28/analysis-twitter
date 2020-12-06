c77a9fa2fffd0c43809a990db8e42db5abb9a279
savacano28 <savacano_28@live.com>
2020-12-06 07:49:18 +0100

    s3,s4

---
+++ poms/jobs/process/s2_stocking_data_into_dw_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/postgresql-42.2.9.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;s2_stocking_data_into_dw_0_1.jar;' analysis_twitter.s2_stocking_data_into_dw_0_1.s2_stocking_data_into_dw  $args
---
+++ poms/jobs/process/s3_stocking_hierarchies_data_into_dw_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/postgresql-42.2.9.jar;../lib/crypto-utils.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;s3_stocking_hierarchies_data_into_dw_0_1.jar; analysis_twitter.s3_stocking_hierarchies_data_into_dw_0_1.s3_stocking_hierarchies_data_into_dw  %*
