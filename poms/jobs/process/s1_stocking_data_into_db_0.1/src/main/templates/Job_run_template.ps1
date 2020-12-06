45dc84c5a72bc260aa373b1a8993eac5067f54f2
savacano28 <savacano_28@live.com>
2020-12-06 20:55:28 +0100

    fix langue

--- poms/jobs/process/s1_stocking_data_into_db_0.1/src/main/templates/Job_run_template.ps1
+++
@@ -1,3 +0,0 @@
-$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-cd $fileDir
-java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/jaxen-1.1.6.jar;../lib/postgresql-42.2.9.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;../lib/accessors-smart-1.1.jar;../lib/jakarta-oro-2.0.8.jar;../lib/json-path-2.1.0.jar;../lib/json-smart-2.2.1.jar;../lib/crypto-utils.jar;s1_stocking_data_into_db_0_1.jar;' analysis_twitter.s1_stocking_data_into_db_0_1.s1_stocking_data_into_db  --context=Default $args
--- poms/jobs/process/s5_fr_calculate_sentiments_from_tweets_0.1/src/main/templates/Job_run_template.bat
+++
@@ -1,3 +0,0 @@
-%~d0
-cd %~dp0
-java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/log4j-1.2-api-2.12.1.jar;../lib/commons-collections-3.2.2.jar;../lib/jboss-serialization.jar;../lib/postgresql-42.2.9.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;../lib/jakarta-oro-2.0.8.jar;../lib/trove.jar;../lib/talendcsv.jar;../lib/crypto-utils.jar;../lib/talend_file_enhanced_20070724.jar;s5_fr_calculate_sentiments_from_tweets_0_1.jar; analysis_twitter.s5_fr_calculate_sentiments_from_tweets_0_1.s5_fr_calculate_sentiments_from_tweets  --context=Default %*
